557162d2cdb7e12bf89636128c1fb741ae86840b
Sebastian Heikamp <sebastian.heikamp@gmail.com>
2018-09-03 13:54:47 -0400

    Completed chapter 3 (backend)

--- social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -1,6 +1,7 @@
 package microservices.book.multiplication.service;
 
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,12 @@ class MultiplicationServiceImpl implements MultiplicationService {
         int factorA = randomGeneratorService.generateRandomFactor();
         int factorB = randomGeneratorService.generateRandomFactor();
         return new Multiplication(factorA, factorB);
+    }
+
+    @Override
+    public boolean checkAttempt(final MultiplicationResultAttempt resultAttempt) {
+        return resultAttempt.getResultAttempt() ==
+                resultAttempt.getMultiplication().getFactorA() *
+                        resultAttempt.getMultiplication().getFactorB();
     }
 }
