f3cfdd3f6fefa4276325ac200e98ec1e0a6c91c8
Sebastian Heikamp <sebastian.heikamp@gmail.com>
2018-09-18 05:34:37 -0400

    Added Swagger (UI) to multiplication & gamification modules

--- gamification/src/main/java/microservices/book/gamification/configuration/WebConfiguration.java
+++ gamification/src/main/java/microservices/book/gamification/configuration/WebConfiguration.java
@@ -1,15 +1,23 @@
 package microservices.book.gamification.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author moises.macero
  */
 @Configuration
 @EnableWebMvc
+@EnableSwagger2
 public class WebConfiguration implements WebMvcConfigurer {
 
     /**
@@ -22,4 +30,21 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**");
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
